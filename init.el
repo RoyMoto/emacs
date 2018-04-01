ff64e453b7cd513dc86599c4e52d17568d31ffbc
motoike <ryo.motoike@gmail.com>
2018-04-01 18:20:26 +0900

    multi-term利用可能に

--- init.el
+++ init.el
@@ -231,9 +231,9 @@
 
 
 ;;multi-termの設定
-;; (when (require 'multi-term nil t)
-;;   ;;使用するシェルを指定
-;;   (setq multi-term-program "/bin/bash"))
+(when (require 'multi-term nil t)
+  ;;使用するシェルを指定
+  (setq multi-term-program "/bin/bash"))
 
 
 (global-set-key "\C-h" 'delete-backward-char)
